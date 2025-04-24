6776bb90ede7e2ee597cb451b6d7791a35cd8ce7
Elijah Kariuki <elijahkariuki1981@outlook.com>
2025-04-23 21:34:23 -0400

    Added RelationalOperators class to demonstrate comparison operators

---
+++ src/com/elijah/operators/RelationalOperators.java
@@ -0,0 +1,22 @@
+package com.elijah.operators;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Equal to (==)
+        System.out.printf("a == b: %b\n", a == b);
+        // Not equal to (!=)
+        System.out.printf("a != b: %b\n", a != b);
+        // Greater than (>)
+        System.out.printf("a > b: %b\n", a > b);
+        // Less than (<)
+        System.out.printf("a < b: %b\n", a < b);
+        // Greater than or equal to (>=)
+        System.out.printf("a >= b: %b\n", a >= b);
+        // Less than or equal to (<=)
+        System.out.printf("a <= b: %b\n", a <= b);
+
+    }
+}
